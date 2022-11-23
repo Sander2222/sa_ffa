28615193759e756aacf9c344d664ecad92521576
Sander <86840128+Sander2222@users.noreply.github.com>
2022-11-23 22:05:26 +0100

    add all SendNuiMessage

--- client/nui.lua
+++ client/nui.lua
@@ -5,3 +5,40 @@ end
 RegisterNUICallback("close", function()
     
 end)
+
+--Join
+ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+    local ActiveGamePlayer = 0
+
+    for i,v in ipairs(ActiveGames) do
+        ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
+    end
+
+    SendNuiMessage({
+        typeS = 'create',
+        ModusA = Config.Modus,
+        MapsA = Config.Maps,
+        ActiveGamePlayerN = ActiveGamePlayer,
+        MaxGamesN= #ActiveGames
+    })
+end)
+
+--Search
+ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+
+    SendNuiMessage({
+        typeS = 'search',
+        ActiveGames = ActiveGames
+    })
+end)
+
+--Join
+SendNuiMessage({
+    typeS = 'join',
+})
+
+--leave
+SendNuiMessage({
+    typeS = 'leave',
+    ClientGame = ActiveClientGame
+})
