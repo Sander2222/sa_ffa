1a35792cccd2e1856ef1504ad3c9bcb4be6dfb8d
Sander <86840128+Sander2222@users.noreply.github.com>
2022-11-23 22:05:01 +0100

    Create svconfig.lua

---
+++ svconfig.lua
@@ -0,0 +1,10 @@
+SvConfig = {}
+
+SvConfig.Webhook = 'https://canary.discord.com/api/webhooks/1001453843227361350/sbbCM02f6Yl0lOrtVmlchPDYEd_EwIPJwVpkZeJJRHVncyX-pH2cYQUmrO81cUc_mLNW'
+SvConfig.WebhookName = 'FFA'
+SvConfig.WebhookColor = 134
+SvConfig.WebhookFooter = 'sa_ffa'
+
+SvConfig.WebhookText = {
+    ['PlayerCreatedGame'] = 'Der Spieler **%s** (%s) hat gerade eine Lobby erstellt: **%s** und Passwort **%s**',
+}
