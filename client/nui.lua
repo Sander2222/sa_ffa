4dd41c64a94506bd75df3ca0026737d2b32b25e4
Sander <86840128+Sander2222@users.noreply.github.com>
2022-11-25 14:42:48 +0100

    add SendNUIMessages and RegisterNUICallbacks

--- client/nui.lua
+++ client/nui.lua
@@ -1,44 +1,94 @@
-function display(bool)
-    SetNuiFocus(bool, bool)
-end
+function display(bool) SetNuiFocus(bool, bool) end
 
-RegisterNUICallback("close", function()
-    
+RegisterNUICallback("close", function() end)
+
+-- --Join
+-- RegisterCommand('ui', function(source, args)
+--     ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+--         local ActiveGamePlayer = 0
+--         local Number = #ActiveGames
+
+--         for i,v in ipairs(ActiveGames) do
+--             ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
+--         end
+
+--         print("lul")
+--         SetNuiFocus(true, true)
+--         SendNUIMessage({
+--             type = "create",
+--             ModusA = Config.Modus,
+--             MapsA = Config.Maps,
+--             ActiveGamePlayerN = ActiveGamePlayer,
+--             MaxGamesN = Number
+--         })
+--     end)
+
+-- --Search
+-- ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+
+--     SendNuiMessage({
+--         type = 'search',
+--         ActiveGames = ActiveGames
+--     })
+-- end)
+
+-- --Join
+-- SendNuiMessage({
+--     type = 'join',
+-- })
+
+-- --leave
+-- SendNuiMessage({
+--     type = 'leave',
+--     ClientGame = ActiveClientGame
+-- })
+
+local AllGames = {}
+
+RegisterCommand('ui', function(source, args)
+    -- Search
+    ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+
+        AllGames = ActiveGames
+
+        SetNuiFocus(true, true)
+        SendNUIMessage({state = 'show', type = 'search'})
+
+        for i, v in ipairs(ActiveGames) do
+            print("1")
+            if tonumber(v.PrivateGame) == 0 and tonumber(v.MaxPlayer) >
+                tonumber(v.Players) then
+                SendNUIMessage({
+                    state = 'add',
+                    type = 'search',
+                    players = v.Players,
+                    maxplayers = v.MaxPlayer,
+                    map = v.Map,
+                    name = v.Name,
+                    modus = v.Modus
+                })
+            end
+        end
+    end)
 end)
 
---Join
-ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
-    local ActiveGamePlayer = 0
+--search
+RegisterNUICallback('JoinSearchedMatch', function(data, cb)
+    print(data.Game)
 
-    for i,v in ipairs(ActiveGames) do
-        ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
+    for i, v in ipairs(AllGames) do
+        if data.Game == v.Name then
+            print("Game gefunden")
+            TriggerServerEvent("sa_ffa:SearchRandomGame", v)
+            print(ESX.DumpTable(v))
+            AllGames = {}
+        end
     end
-
-    SendNuiMessage({
-        type = 'create',
-        ModusA = Config.Modus,
-        MapsA = Config.Maps,
-        ActiveGamePlayerN = ActiveGamePlayer,
-        MaxGamesN= #ActiveGames
-    })
 end)
 
---Search
-ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
-
-    SendNuiMessage({
-        type = 'search',
-        ActiveGames = ActiveGames
+RegisterNUICallback('exit', function(data, cb)
+    SetNuiFocus(false, false)
+    SendNUIMessage({
+        state = 'close'
     })
 end)
-
---Join
-SendNuiMessage({
-    type = 'join',
-})
-
---leave
-SendNuiMessage({
-    type = 'leave',
-    ClientGame = ActiveClientGame
-})
