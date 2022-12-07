7d764fc3814fc41441f95954a7282f1260329d21
Sander <86840128+Sander2222@users.noreply.github.com>
2022-12-07 20:29:07 +0100

    ich habs gefixt glaube ich lol

--- client/nui.lua
+++ client/nui.lua
@@ -1,46 +1,27 @@
-function display(bool) SetNuiFocus(bool, bool) end
-
-RegisterNUICallback("close", function() end)
-
--- --Join
--- RegisterCommand('ui', function(source, args)
---     ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
---         local ActiveGamePlayer = 0
---         local Number = #ActiveGames
-
---         for i,v in ipairs(ActiveGames) do
---             ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
---         end
-
---         print("lul")
---         SetNuiFocus(true, true)
---         SendNUIMessage({
---             type = "create",
---             ModusA = Config.Modus,
---             MapsA = Config.Maps,
---             ActiveGamePlayerN = ActiveGamePlayer,
---             MaxGamesN = Number
---         })
---     end)
-
-
-
--- --Join
--- SendNuiMessage({
---     type = 'join',
--- })
-
--- --leave
--- SendNuiMessage({
---     type = 'leave',
---     ClientGame = ActiveClientGame
--- })
-
 local AllGames = {}
 
-RegisterCommand('ui', function(source, args)
-    local Map
-    -- Search
+function FFAUICreate()
+    ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+        local ActiveGamePlayer = 0
+        local Number = #ActiveGames
+
+        for i,v in ipairs(ActiveGames) do
+            ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
+        end
+
+        print("lul")
+        SetNuiFocus(true, true)
+        SendNUIMessage({
+            type = "create",
+            ModusA = Config.Modus,
+            MapsA = Config.Maps,
+            ActiveGamePlayerN = ActiveGamePlayer,
+            MaxGamesN = Number
+        })
+    end)
+end
+
+function FFAUISearch()
     ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
 
         AllGames = ActiveGames
@@ -70,7 +51,7 @@ RegisterCommand('ui', function(source, args)
             end
         end
     end)
-end)
+end
 
 --search Callback
 RegisterNUICallback('JoinSearchedMatch', function(data, cb)
