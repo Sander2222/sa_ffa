eba8f966ce5504b67360811e180098e29f4b53e5
Sander <86840128+Sander2222@users.noreply.github.com>
2022-12-07 20:19:30 +0100

    Revert "Update nui.lua"
    This reverts commit ca224037ae5dd9bf5443f3752195865454902062.

--- client/nui.lua
+++ client/nui.lua
@@ -1,3 +1,30 @@
+function display(bool) SetNuiFocus(bool, bool) end
+
+RegisterNUICallback("close", function() end)
+
+-- --Join
+-- RegisterCommand('ui', function(source, args)
+--     ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
+--         local ActiveGamePlayer = 0
+--         local Number = #ActiveGames
+
+--         for i,v in ipairs(ActiveGames) do
+--             ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
+--         end
+
+--         print("lul")
+--         SetNuiFocus(true, true)
+--         SendNUIMessage({
+--             type = "create",
+--             ModusA = Config.Modus,
+--             MapsA = Config.Maps,
+--             ActiveGamePlayerN = ActiveGamePlayer,
+--             MaxGamesN = Number
+--         })
+--     end)
+
+
+
 -- --Join
 -- SendNuiMessage({
 --     type = 'join',
@@ -11,26 +38,11 @@
 
 local AllGames = {}
 
-function FFAUICreate()
-    ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
-        local ActiveGamePlayer = 0
-        local Number = #ActiveGames
+RegisterCommand('ui', function(source, args)
 
-        for i,v in ipairs(ActiveGames) do
-            ActiveGamePlayer = ActiveGamePlayer + ActiveGames.Players
-        end
+    -- Search
 
-        print("lul")
-        SetNuiFocus(true, true)
-        SendNUIMessage({
-            type = "create",
-            ModusA = Config.Modus,
-            MapsA = Config.Maps,
-            ActiveGamePlayerN = ActiveGamePlayer,
-            MaxGamesN = Number
-        })
-    end)
-end
+end)
 
 function FFAUISearch()
     ESX.TriggerServerCallback('sa_ffa:GetAllGames', function(ActiveGames)
