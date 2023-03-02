dbe55ade939acacbeb1e361f2caff96954b58b55
Sander <heinzwalter0987@gmail.com>
2023-03-02 23:22:53 +0100

    add locals to config.js

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
 ConfigData = []
 
-ConfigData.Info = "lol"
+ConfigData.Info = 'lol'
+ConfigData.BlacklistedWords = ['nigger', 'nigga', 'niggers', 'niger', 'hitler', 'adolf', 'penis', 'hurensohn', 'nutte', 'schwanz', 'pedo', 'milf', 'hitl', 'nega', 'negga', 'porn', 'porno', 'nazi', 'anal', 'shit', 'neonazi', 'huan', 'huansohn', 'hure']
+
+ConfigData.Locals = {
+    'Name': 'Name',
+    'Password': 'Password',
+    'CreateFFAText': 'Create Your FFA',
+    'EnterData': 'Enter here',
+    'MaxPlayer': 'Max Player',
+    'ChooseMode': 'Choose Mode',
+    'ChooseMap': 'Choose Map',
+    'CreateFFA': 'Create FFA'
+}
