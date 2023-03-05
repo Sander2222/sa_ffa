53b7dcc2403e4b78374b62c36b645abe39117686
Sander <heinzwalter0987@gmail.com>
2023-03-05 02:09:31 +0100

    add comments

--- svconfig.lua
+++ svconfig.lua
@@ -1,13 +1,16 @@
 SvConfig = {}
 
+-- This Webhook is where the logs get send
 SvConfig.WebhookLogs = 'https://canary.discord.com/api/webhooks/1001453843227361350/sbbCM02f6Yl0lOrtVmlchPDYEd_EwIPJwVpkZeJJRHVncyX-pH2cYQUmrO81cUc_mLNW'
 SvConfig.WebhookNameLogs = 'FFA'
 SvConfig.WebhookColor = 134
 SvConfig.WebhookFooter = 'sa_ffa'
 
+-- This Webhook is where the Scorenboard get send
 SvConfig.WebhookScoreboard = 'https://canary.discord.com/api/webhooks/1001453843227361350/sbbCM02f6Yl0lOrtVmlchPDYEd_EwIPJwVpkZeJJRHVncyX-pH2cYQUmrO81cUc_mLNW'
 SvConfig.WebhookNameLogsScoreboard = 'FFA Scoreboard'
 
+-- Webhook locals
 SvConfig.WebhookText = {
     ['PlayerCreatedGame'] = 'Der Spieler **%s** (%s) hat gerade eine Lobby erstellt. Infos zu der Lobby: \n\nName: %s \nPasswort: %s',
     ['PlayerJoinedRoom'] = 'Der Spieler **%s** (%s) ist den %s gejoint',
