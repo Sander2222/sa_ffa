813c800a7c640e4fa25b1ac0ae707eb3ec8289ac
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-03 17:10:44 +0200

    add Config.ChezzaRessourceName

--- config.lua
+++ config.lua
@@ -14,7 +14,8 @@ Config.ShowleaveCommandNotify = true -- This is if the player get a notify how t
 Config.Dist = 3.0 -- the distance to check at the point
 Config.DisableBlip = true -- put this on true if you want do disable all blips if the player is in a ffa game (beta version)
 Config.UseESX12 = false -- not recommended and not tested!!! (if you use 1.2 or lower please update your ESX Version)
-Config.UseChezza = false --Put this on true if you use  Chezza Inventory
+Config.UseChezza = true --Put this on true if you use  Chezza Inventory
+Config.ChezzaRessourceName = 'inventfory'
 Config.UseOXInventory = false -- if you use ox_inventory put this on true
 
 
